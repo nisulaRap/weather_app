f6c19597499a571c6d33b2992e75cff4bce420f4
nisulaRap <nisula.rap@gmail.com>
2025-01-31 23:53:11 +0530

    added getWeatherIcon and getCurrentDate functions

--- script.js
+++ script.js
@@ -40,6 +40,27 @@ async function getFetchData(endPoint, city) {
     return response.json();
 }
 
+function getWeatherIcon(id) {
+    if (id <= 232) return 'thunderstorm.svg';
+    if (id <= 321) return 'drizzle.svg';
+    if (id <= 531) return 'rain.svg';
+    if (id <= 622) return 'snow.svg';
+    if (id <= 781) return 'atmosphere.svg';
+    if (id === 800) return 'clear.svg';
+    return 'sun_with_cloud.svg';
+}
+
+function getCurrentDate() {
+    const currentDate = new Date();
+    const options = {
+        weekday: 'short',
+        day: '2-digit',
+        month: 'short',
+    };
+
+    return currentDate.toLocaleDateString('en-GB', options);
+}
+
 async function updateWeatherInfo(city) {
     const weatherData = await getFetchData('weather', city);
 
