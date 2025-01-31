edd35645bd310fb4937f7bca55a933b94bd1a208
nisulaRap <nisula.rap@gmail.com>
2025-01-31 21:58:45 +0530

    added variables

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
 const cityInput = document.querySelector('.city-input');
 const searchBtn = document.querySelector('.search-btn');
 
+const weatherInfoSection = document.querySelector('.weather-info');
+const notFoundSection = document.querySelector('.not-found');
+const searchCitySection = document.querySelector('.search-city');
+
+const countryTxt = document.querySelector('.country-txt');
+const tempTxt = document.querySelector('.temp-txt');
+const conditionTxt = document.querySelector('.condition-txt');
+const humidityValueTxt = document.querySelector('.humidity-value-txt');
+const windValueTxt = document.querySelector('.wind-value-txt');
+const weatherSummaryImg = document.querySelector('.weather-summary-img');
+const currentDateTxt = document.querySelector('.current-date-txt');
+
+const forecastItemsContainer = document.querySelector('.forecast-items-container');
+
 const apikey = '43601d0bb0776bce11cbc30bf5bcd42a';
 
 searchBtn.addEventListener('click', () => {
