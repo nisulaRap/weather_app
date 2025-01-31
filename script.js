b195332d92982d305d6dff1ad407f08bd822c993
nisulaRap <nisula.rap@gmail.com>
2025-01-31 21:55:13 +0530

    added API key

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const cityInput = document.querySelector('.city-input');
 const searchBtn = document.querySelector('.search-btn');
 
+const apikey = '43601d0bb0776bce11cbc30bf5bcd42a';
+
 searchBtn.addEventListener('click', () => {
     if (cityInput.value.trim() !== '') {
         updateWeatherInfo(cityInput.value);
